d237293ef8a3f384bf9cdc5260b51f9eef657fff
ilya <ilya.krapivnoy@gmail.com>
2021-10-09 22:58:06 +0300

    create components and add css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-    return <div>Hello World</div>;
+import React from 'react';
+
+import { Cards, CountryPicker, Chart } from './components';
+import styles from './App.module.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+        );
+    }
 }
 
 export default App;
