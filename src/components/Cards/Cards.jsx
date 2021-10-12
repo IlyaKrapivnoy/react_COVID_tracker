75d5ff93da42f155b703f9c91c91cdef905d2f93
ilya <ilya.krapivnoy@gmail.com>
2021-10-12 17:27:02 +0300

    get real data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,20 +1,47 @@
 import styles from './Cards.module.css';
 import { Card, Grid, CardContent, Typography } from '@material-ui/core';
 
-const Cards = (props) => {
-    console.log(props);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if (!confirmed) {
+        return 'Loading';
+    }
+
     return (
         <div className={styles.container}>
-            <Grid container spacing={3} justify='center'>
+            <Grid container spacing={3} justifyContent='center'>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>
                             Infected
                         </Typography>
-                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography variant='h5'>{confirmed.value}</Typography>
                         <Typography color='textSecondary'>REAL DATA</Typography>
                         <Typography variant='body2'>
                             Number of active cases of COVID-19
+                        </Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>
+                            Recovered
+                        </Typography>
+                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography color='textSecondary'>REAL DATA</Typography>
+                        <Typography variant='body2'>
+                            Number of recoveries from COVID-19
+                        </Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>
+                            Deaths
+                        </Typography>
+                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography color='textSecondary'>REAL DATA</Typography>
+                        <Typography variant='body2'>
+                            Number of deaths cases caused by COVID-19
                         </Typography>
                     </CardContent>
                 </Grid>
