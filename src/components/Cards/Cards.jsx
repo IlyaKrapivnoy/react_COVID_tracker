468178e9f0152b1e150e662208f1aa67ea143be5
ilya <ilya.krapivnoy@gmail.com>
2021-10-12 17:36:00 +0300

    use real data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -36,8 +36,17 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography color='textSecondary' gutterBottom>
                             Recovered
                         </Typography>
-                        <Typography variant='h5'>REAL DATA</Typography>
-                        <Typography color='textSecondary'>REAL DATA</Typography>
+                        <Typography variant='h5'>
+                            <CountUp
+                                start={0}
+                                end={recovered.value}
+                                duration={2}
+                                separator=','
+                            />
+                        </Typography>
+                        <Typography color='textSecondary'>
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>
                         <Typography variant='body2'>
                             Number of recoveries from COVID-19
                         </Typography>
@@ -48,8 +57,17 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography color='textSecondary' gutterBottom>
                             Deaths
                         </Typography>
-                        <Typography variant='h5'>REAL DATA</Typography>
-                        <Typography color='textSecondary'>REAL DATA</Typography>
+                        <Typography variant='h5'>
+                            <CountUp
+                                start={0}
+                                end={deaths.value}
+                                duration={2}
+                                separator=','
+                            />
+                        </Typography>
+                        <Typography color='textSecondary'>
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>
                         <Typography variant='body2'>
                             Number of deaths cases caused by COVID-19
                         </Typography>
