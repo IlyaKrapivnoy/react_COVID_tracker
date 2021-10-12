235e5e6cc77f1971b948c0d8254498e3273858dc
ilya <ilya.krapivnoy@gmail.com>
2021-10-12 18:11:21 +0300

    work with datasets

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+import styles from './Charts.module.css';
 
 const Chart = () => {
     const [dailyData, setDailyData] = useState({});
@@ -8,8 +10,32 @@ const Chart = () => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         };
+        console.log(dailyData);
         fetchAPI();
     });
+
+    const lineChart = dailyData[0] ? (
+        <Line
+            data={{
+                labels: dailyData(({ date }) => date),
+                datasets: [
+                    {
+                        data: dailyData(({ confirmed }) => confirmed),
+                        label: 'Infected',
+                        borderColor: '#3333ff',
+                        fill: true,
+                    },
+                    {
+                        data: dailyData(({ deaths }) => deaths),
+                        label: 'Deaths',
+                        borderColor: 'red',
+                        backgroundColor: 'rgba(255, 0,0,.5)',
+                        fill: true,
+                    },
+                ],
+            }}
+        />
+    ) : null;
 
     return <h2>Chart</h2>;
 };
